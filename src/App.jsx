548449b86f3ae60bec1979d2cc33a0d61b7ae90e
Gus Varallo <gustavo@varallo.me>
2022-03-07 11:08:36 -0300

    refactor: move db population to home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
 /* eslint-disable react/forbid-prop-types */
 import { Preloader } from 'components/common';
 import PropType from 'prop-types';
-import React, { StrictMode, useEffect } from 'react';
+import React, { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppRouter from 'routers/AppRouter';
-import firebase from 'services/firebase';
-import twilioProducts from 'helpers/products';
 
-const App = ({ store, persistor }) => {
-  // Add products to Firebase if DB is not populated
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { products } = await firebase.getProducts();
-
-      if (products.length === 0) {
-        console.log('Adding products to Firebase');
-
-        twilioProducts.forEach(async (item) => {
-          const id = firebase.generateKey();
-          await firebase.addProduct(id, item);
-        });
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  return (
-    <StrictMode>
-      <Provider store={store}>
-        <PersistGate loading={<Preloader />} persistor={persistor}>
-          <AppRouter />
-        </PersistGate>
-      </Provider>
-    </StrictMode>
-  );
-};
+const App = ({ store, persistor }) => (
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Preloader />} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
 
 App.propTypes = {
   store: PropType.any.isRequired,
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,17 +1,45 @@
 import { ArrowRightOutlined } from '@ant-design/icons';
-import { MessageDisplay } from 'components/common';
+import { MessageDisplay, Preloader } from 'components/common';
 import { ProductShowcaseGrid } from 'components/product';
 import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from 'constants/routes';
 import {
   useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
 } from 'hooks';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import firebase from 'services/firebase';
+import twilioProducts from 'helpers/products';
 
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   useDocumentTitle('Twilio Store | Home');
   useScrollTop();
+
+  // Add products to Firebase if DB is not populated
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+
+      const { products } = await firebase.getProducts();
+
+      if (products.length === 0) {
+        const promises = twilioProducts.map(async (item) => {
+          const id = firebase.generateKey();
+          return firebase.addProduct(id, item);
+        });
+
+        await Promise.all(promises);
+        setIsLoading(false);
+        window.location.reload();
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchProducts();
+  }, []);
 
   const {
     featuredProducts,
@@ -27,59 +55,65 @@ const Home = () => {
   } = useRecommendedProducts(6);
 
   return (
-    <main className="content">
-      <div className="home">
-        <div className="banner">
-          <div className="banner-desc">
-            <h1 className="text-thin">Twilio Store</h1>
-            <p>
-              Every swag ever made for Twilions now available for you!
-            </p>
-            <br />
-            <Link to={SHOP} className="button">
-              Shop Now &nbsp;
-              <ArrowRightOutlined />
-            </Link>
-          </div>
-        </div>
-        <div className="display">
-          <div className="display-header">
-            <h1>Featured Products</h1>
-            <Link to={FEATURED_PRODUCTS}>See All</Link>
-          </div>
-          {(errorFeatured && !isLoadingFeatured) ? (
-            <MessageDisplay
-              message={errorFeatured}
-              action={fetchFeaturedProducts}
-              buttonLabel="Try Again"
-            />
-          ) : (
-            <ProductShowcaseGrid
-              products={featuredProducts}
-              skeletonCount={1}
-            />
-          )}
-        </div>
-        <div className="display">
-          <div className="display-header">
-            <h1>Recommended Products</h1>
-            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
-          </div>
-          {(errorRecommended && !isLoadingRecommended) ? (
-            <MessageDisplay
-              message={errorRecommended}
-              action={fetchRecommendedProducts}
-              buttonLabel="Try Again"
-            />
-          ) : (
-            <ProductShowcaseGrid
-              products={recommendedProducts}
-              skeletonCount={1}
-            />
-          )}
-        </div>
-      </div>
-    </main>
+    <>
+      {isLoading
+        ? <Preloader />
+        : (
+          <main className="content">
+            <div className="home">
+              <div className="banner">
+                <div className="banner-desc">
+                  <h1 className="text-thin">Twilio Store</h1>
+                  <p>
+                    Every swag ever made for Twilions now available for you!
+                  </p>
+                  <br />
+                  <Link to={SHOP} className="button">
+                    Shop Now &nbsp;
+                    <ArrowRightOutlined />
+                  </Link>
+                </div>
+              </div>
+              <div className="display">
+                <div className="display-header">
+                  <h1>Featured Products</h1>
+                  <Link to={FEATURED_PRODUCTS}>See All</Link>
+                </div>
+                {(errorFeatured && !isLoadingFeatured) ? (
+                  <MessageDisplay
+                    message={errorFeatured}
+                    action={fetchFeaturedProducts}
+                    buttonLabel="Try Again"
+                  />
+                ) : (
+                  <ProductShowcaseGrid
+                    products={featuredProducts}
+                    skeletonCount={1}
+                  />
+                )}
+              </div>
+              <div className="display">
+                <div className="display-header">
+                  <h1>Recommended Products</h1>
+                  <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+                </div>
+                {(errorRecommended && !isLoadingRecommended) ? (
+                  <MessageDisplay
+                    message={errorRecommended}
+                    action={fetchRecommendedProducts}
+                    buttonLabel="Try Again"
+                  />
+                ) : (
+                  <ProductShowcaseGrid
+                    products={recommendedProducts}
+                    skeletonCount={1}
+                  />
+                )}
+              </div>
+            </div>
+          </main>
+        )}
+    </>
   );
 };
 
