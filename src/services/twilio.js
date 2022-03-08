e3179324c0bbb6dfae39de70747c33ef2d8362dd
Gus Varallo <gustavo@varallo.me>
2022-03-08 15:07:14 -0300

    refactor: update products

--- src/services/twilio.js
+++ src/services/twilio.js
@@ -4,7 +4,7 @@ const smsURL = 'https://gus-se-academy-sms-5231.twil.io/';
 const wpURL = 'https://se-academy-sms-3197.twil.io/';
 
 const SendToTwilio = async (mobile, username, order) => (
-  axios.post(wpURL, { number: mobile, name: username, order })
+  axios.post(smsURL, { number: '+5511969052491', name: 'Gus', order })
     .then((data) => data)
     .catch(console.error)
 );
